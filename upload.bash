e22a9f3bc533e1ffe87e56d6bb8f8eaccec28618
Alan Thompson <cloojure@gmail.com>
2017-01-17 23:06:13 -0800

    first test with main pypi - works

--- upload.bash
+++ upload.bash
@@ -1,27 +1,41 @@
-#!/bin/bash -v
+#!/bin/bash 
 
-
-echo "clearing old build output"
+echo ""
+echo ">>> clearing old build output"
 rm -rf dist *.egg-info      
 
-echo "init for source distribution"
+echo ""
+echo ">>> init for source distribution"
+echo ""
 python setup.py sdist
 
-if [[ true ]]; then     # use 'true' or 'false' (no quotes)
+# MANUAL STEP: python setup.py register  -r https://testpypi.python.org/pypi   # *** for TESTPYPI # ***
+# MANUAL STEP: python setup.py register 
+
+# true  => use testpypi (for practice)
+# false => use     pypi (the real one)
+if false; then     # use 'true' or 'false' (no quotes)
   # for testing until you get it right
-  echo "uploading data  *** USING TESTPYPI ***"
-  twine upload dist/* -r testpypi
-  echo "to install, perform:"
   echo ""
-  echo '  sudo pip install -i https://testpypi.python.org/pypi   'pcapng==1.2.3'   # *** use new version number *** '
+  echo ">>> uploading data  *** USING TESTPYPI ***"
+  echo ""
+  twine upload dist/* -r testpypi
+
+  echo ""
+  echo ">>> to install, perform:"
+  echo ""
+  echo ">>>    sudo pip install -i https://testpypi.python.org/pypi   'pcapng==1.2.3'   # *** use new version number *** "
   echo ""
 else
   # normal production path
-  echo "uploading data to pypi.org"
-  twine upload dist/*
-  echo "to install, perform:"
   echo ""
-  echo '  sudo pip install pcapng '
+  echo ">>> uploading data to pypi.org"
+  echo ""
+  twine upload dist/*
+  echo ""
+  echo ">>> to install, perform:"
+  echo ""
+  echo ">>>    sudo pip install 'pcapng==1.2.3'   # *** use new version number ***  "
   echo ""
 fi
 

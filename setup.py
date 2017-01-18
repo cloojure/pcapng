b14a2c6b8720c3d853c6f1ed43ff938803666252
Alan Thompson <cloojure@gmail.com>
2017-01-17 22:08:49 -0800

    misc

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['pcapng', 'tests']),
+    packages=find_packages( exclude=['tests'] ),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:

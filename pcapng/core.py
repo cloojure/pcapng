51067e513b41e7723f0df7062f5474085d60174a
Alan Thompson <cloojure@gmail.com>
2017-01-19 14:05:59 -0800

    misc

--- pcapng/core.py
+++ pcapng/core.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #todo add brocade copyright / license
 import struct
 import pcapng.linktype
--- pcapng/option.py
+++ pcapng/option.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 # http://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi?url=https://raw.githubusercontent.com/pcapng/pcapng/master/draft-tuexen-opsawg-pcapng.xml&modeAsFormat=html/ascii&type=ascii#formatopt
 

9865083f5de8a11744f4250a86f08bf02721fec1
Alan Thompson <cloojure@gmail.com>
2017-01-19 20:18:34 -0800

    misc

--- pcapng/util.py
+++ pcapng/util.py
@@ -12,9 +12,9 @@ def is_python3():
     (major, minor, micro, release_level, serial) = sys.version_info
     return ((major == 3) and (minor >= 5))
 
-
 def assert_python2():
     assert is_python2()
+
 
 def assert_type_bytearray( arg ):
     assert type( arg ) == bytearray
@@ -39,32 +39,41 @@ def assert_int8(arg):          # signed byte
 
 
 def to_bytes( arg ):
+    """Converts arg to a 'bytes' object."""
     return bytes( bytearray( arg ))    # if python2, 'bytes' is synonym for 'str'
 
 def str_to_bytes( arg ):
+    """Converts a string arg to a 'bytes' object."""
+    assert_type_str( arg )
     """Convert an ASCII string to 'bytes'. Works on both Python2 and Python3."""
     return to_bytes( map(ord,arg))
 
 
 #todo move to pcap
 def fmt_pcap_hdr( ts_sec, ts_usec, incl_len, orig_len ):
+    """Format a PCAP block header."""
     packed = struct.pack( '>LLLL', ts_sec, ts_usec, incl_len, orig_len)
     return packed
 
+
 def split_float( fval ):
+    """Splits a float into integer and fractional parts."""
     frac, whole = math.modf( fval )
     micros = int( round( frac * 1000000 ))
     return int(whole), micros
 
 def curr_utc_time_tuple():
+    """Returns the current UTC time as a (secs, usecs) tuple."""
     utc_secs = time.time()
     secs, usecs = split_float( utc_secs )
     return secs, usecs
 
 def timetup_to_float( secs, usecs ):
+    """Converts a time tuple from (secs, usecs) to float."""
     return secs + (usecs / 1000000.0)
 
 def timetup_subtract( ts1, ts2 ):
+    """Subtracts two time tuples in (secs, usecs) format, returning a float result."""
     (s1, us1) = ts1
     (s2, us2) = ts2
     t1 = timetup_to_float( s1, us1 )
@@ -80,6 +89,7 @@ def ChrList_to_str(arg):
 
 #todo move to pcapng.list (or delete?); only(), second(), last(), butlast(), rest()
 def first( lst ):
+    """Returns the first item in a sequence."""
     return lst[0]
 
 #todo move to pcapng.bytes

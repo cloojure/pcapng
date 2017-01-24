df2b6cfcdcf7dfdfbf2fd4e57b05b447ab926f59
Alan Thompson <cloojure@gmail.com>
2017-01-24 13:37:18 -0800

    misc

--- isis_demo.py
+++ isis_demo.py
@@ -6,7 +6,6 @@ import pcapng.core
 import pcapng.mrt
 from   pcapng.util import to_bytes
 
-out_file_name = 'isis.pcapng'
 pkt_len_min     = 13
 pkt_len_max     = 27
 pkt_len_curr    = -1
@@ -22,6 +21,7 @@ def get_pkt():
     return result
 
 
+out_file_name = 'isis.pcapng'
 print('\n')
 print("Saving sample ISIS packets to file:   %s" % out_file_name)
 pcap_fp = open( out_file_name, 'wb' )
@@ -30,9 +30,24 @@ for count in range(20):
     pkt_data = get_pkt()
 
     packed_bytes = pcapng.core.custom_block_pack(
-                        pcapng.core.CUSTOM_BLOCK_COPYABLE, pcapng.core.BROCADE_PEN,
-                        pcapng.mrt.mrt_isis_block_pack( pkt_data ))
+        pcapng.core.CUSTOM_BLOCK_COPYABLE, pcapng.core.BROCADE_PEN,
+        pcapng.mrt.mrt_isis_block_pack( pkt_data ))
 
+    pcap_fp.write( packed_bytes )
+    time.sleep(0.1)
+    print( '.', end='' )
+    sys.stdout.flush()
+    count -= 1
+print('\n\n')
+
+
+out_file_name = 'isis.mrt'
+print("Saving sample ISIS packets to file:   %s" % out_file_name)
+pcap_fp = open( out_file_name, 'wb' )
+
+for count in range(20):
+    pkt_data = get_pkt()
+    packed_bytes = pcapng.mrt.mrt_isis_block_pack( pkt_data )
     pcap_fp.write( packed_bytes )
     time.sleep(0.1)
     print( '.', end='' )

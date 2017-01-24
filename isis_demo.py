b034c712b2f33bc587a5b7776ceb686ac557fa5d
Alan Thompson <cloojure@gmail.com>
2017-01-24 13:19:32 -0800

    misc

--- isis_demo.py
+++ isis_demo.py
@@ -6,6 +6,7 @@ import pcapng.core
 import pcapng.mrt
 from   pcapng.util import to_bytes
 
+out_file_name = 'isis.pcapng'
 pkt_len_min     = 13
 pkt_len_max     = 27
 pkt_len_curr    = -1
@@ -20,13 +21,17 @@ def get_pkt():
     result = to_bytes( range( pkt_len_min, pkt_len_curr+1 ))
     return result
 
-print("Saving sample ISIS packets")
-pcap_fp = open( 'isis.pcapng', 'wb' )
+print('\n')
+print("Saving sample ISIS packets to file:   %s" % out_file_name)
+pcap_fp = open( out_file_name, 'wb' )
 pcap_fp.write( pcapng.core.section_header_block_pack() )
-while True:
+count = 20
+while (count > 0):
     pkt_data = get_pkt()
     packed_bytes = pcapng.mrt.mrt_isis_block_pack( pkt_data )
     pcap_fp.write( packed_bytes )
-    time.sleep(0.2)
+    time.sleep(0.1)
     print( '.', end='' )
     sys.stdout.flush()
+    count -= 1
+print('\n\n')

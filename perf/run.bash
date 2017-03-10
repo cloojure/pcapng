8a8bf003b40435226e96943b0ed23d6dd844460a
Alan Thompson <cloojure@gmail.com>
2017-03-10 13:42:11 -0800

    First run of BPF in docker!

---
+++ perf/run.bash
@@ -0,0 +1,7 @@
+#!/bin/bash -v
+docker run --privileged -it --name=perf-cont --net host --pid=host \
+  -v /usr/src:/usr/src \
+  -v /lib/modules:/lib/modules \
+  -v /sys/kernel/debug:/sys/kernel/debug \
+  perf-1 \
+  /bin/bash

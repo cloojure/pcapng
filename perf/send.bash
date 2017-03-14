d68a3ae7e811fbe24a5d315e56f2df5d4f536d6c
Alan Thompson <cloojure@gmail.com>
2017-03-13 20:45:04 -0700

    misc

---
+++ perf/send.bash
@@ -0,0 +1,18 @@
+sudo tcpreplay --intf1=enp0s3  --pps=100 --loop=100 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --pps=topspeed --loop=100 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --pps=maxspeed --loop=100 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=100 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=1000 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=2000 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=5000 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=4000 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=3500 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=3000 --enable-file-cache  ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=3000 --enable-file-cache  ISIS_level2_adjacency.cap --quiet
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=3000 --enable-file-cache --quiet   ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --topspeed --loop=4000 --enable-file-cache --quiet   ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --loop  --pps=50000 --enable-file-cache --quiet   ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --pps=10000 --loop=1000 --enable-file-cache --quiet   ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --pps=20000 --loop=1000 --enable-file-cache --quiet   ISIS_level2_adjacency.cap
+sudo tcpreplay --intf1=enp0s3  --mbps=100 --loop=1000 --enable-file-cache --quiet   ISIS_level2_adjacency.cap
+
